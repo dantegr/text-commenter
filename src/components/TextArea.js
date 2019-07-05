de090752b1f7c31fa8bda88d684c1f3b373aac08
Pantelis <tampantelis@gmail.com>
2019-07-05 20:26:16 +0300

    first commit. contains some initial components

---
+++ src/components/TextArea.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TextArea = () => {
+  return (
+    <div className='textBox'>
+      <h3>This is sample lorem ipsum text</h3>
+      <p>Lorem ipsum dolor sit amet, tempor labore molestiae eum no, mea brute dolorem adipisci et, ne quo putent dignissim. Id scripta placerat pertinacia vel, noster nominavi menandri mel cu. An nullam facilis mnesarchum pro. Cu graeco deterruisset nam, no cum erat habeo accumsan. Quo posse liber ea, minim apeirian ea vix.</p>
+      <p>Et ullum eruditi constituto ius, menandri sensibus ius no. Ne populo ullamcorper per, at cetero aperiam vis. Id pri ridens delectus, mundi legere dignissim ut nec. Ad accusam noluisse cum, vide accumsan sed in. Ut duo zril sensibus interpretaris, sea maiorum abhorreant no.</p>
+    </div>
+  );
+}
+
+export default TextArea;
